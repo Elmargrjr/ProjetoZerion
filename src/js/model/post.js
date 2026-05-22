6a84a048b17abe24e6c895d79919e83ade3d5ec3
Donelli0 <vinidoneli@gmail.com>
2026-05-22 19:55:00 -0300

    Adicionando novas classes no model

---
+++ src/js/model/post.js
@@ -0,0 +1,10 @@
+class Post {
+    constructor(id, conteudo, usuario_id) {
+        this.id = id;
+        this.conteudo = conteudo;
+        this.usuario_id = usuario_id;
+
+    }
+}
+
+module.exports = Post;
---
+++ src/js/model/seguidores.js
@@ -0,0 +1,9 @@
+class Seguidores {
+    constructor(id, seguidor_id, seguindo_id) {
+        this.id = id;
+        this.seguidor_id = seguidor_id;
+        this.seguindo_id = seguindo_id;
+    }
+}
+
+module.exports = Seguidores;
